24414850462f5f77ac2947abf218c41b62132be1
sukranyorulmazzzz <sukran_yrlmz@hotmail.com>
2024-06-06 15:09:47 +0300

    .

--- src/App.vue
+++ src/App.vue
@@ -14,37 +14,6 @@
         <div class="loader-1 bar"></div>
         <div class="loader-2 bar"></div>
       </div>
-      <div class="counter">
-        <div class="counter-1 digit">
-          <div class="num">0</div>
-          <div class="num num1offset1">1</div>
-        </div>
-        <div class="counter-2 digit">
-          <div class="num">0</div>
-          <div class="num num1offset2">1</div>
-          <div class="num">2</div>
-          <div class="num">3</div>
-          <div class="num">4</div>
-          <div class="num">5</div>
-          <div class="num">6</div>
-          <div class="num">7</div>
-          <div class="num">8</div>
-          <div class="num">9</div>
-          <div class="num">0</div>
-        </div>
-        <div class="counter-3 digit">
-          <div class="num">0</div>
-          <div class="num">1</div>
-          <div class="num">2</div>
-          <div class="num">3</div>
-          <div class="num">4</div>
-          <div class="num">5</div>
-          <div class="num">6</div>
-          <div class="num">7</div>
-          <div class="num">8</div>
-          <div class="num">9</div>
-        </div>
-      </div>
     </div>
   </div>
 </template>
@@ -60,21 +29,6 @@ export default {
   },
   methods: {
     initCounter() {
-      const counter3 = this.$el.querySelector(".counter-3");
-
-      for (let i = 0; i < 2; i++) {
-        for (let j = 0; j < 10; j++) {
-          const div = document.createElement("div");
-          div.className = "num";
-          div.textContent = j;
-          counter3.appendChild(div);
-        }
-      }
-      const finalDiv = document.createElement("div");
-      finalDiv.className = "num";
-      finalDiv.textContent = "0";
-      counter3.appendChild(finalDiv);
-
       function animate(counter, duration, delay = 0) {
         const numHeight = counter.querySelector(".num").clientHeight;
         const totalDistance =
@@ -86,10 +40,6 @@ export default {
           ease: "power2.inOut",
         });
       }
-
-      animate(counter3, 5);
-      animate(this.$el.querySelector(".counter-2"), 6);
-      animate(this.$el.querySelector(".counter-1"), 2, 4);
     },
     initAnimations() {
       gsap.to(".digit", {
